ca710835393199cb72db8c862832598f42765072
Huanle Zhang <zhanghuanle1342@gmail.com>
2017-10-16 14:58:10 -0700

    dtc-show-ip draft

---
+++ interface.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title> Huanle Zhang's Personal Website </title>
+		
+		<meta charset="UTF-8">
+		<meta name="description" content="Homepage">
+		<meta name="keywords" content="Huanle Zhang, Biography, Homepage">
+		<meta name="author" content="Huanle Zhang">
+		<meta http-equiv="refresh" content="60"> <!-- refresh every 60s -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<link rel="shortcut icon" href="favicon.ico" /> <!-- Icon -->
+		
+		<script src="js/jquery-3.2.1.js"></script>
+		
+		<link rel="stylesheet" href="css/theme.css" />
+		<link rel="stylesheet" href="css/header.css" />
+		<link rel="stylesheet" href="css/footer.css" />
+		
+		<link rel="stylesheet" href="css/home.css" />
+		
+	</head>
+	
+	<body>
+		
+		<?php include 'header.php'; ?>
+
+		
+		<section id="content_start">
+			
+			<!-- display -->
+			
+		</section>
+		
+		<?php include 'footer.php'; ?>
+		
+	</body>
+
+</html>
