51cd12230ed3c6af39d401b5ff2ad932f2743494
Huanle Zhang <zhanghuanle1342@gmail.com>
2017-10-16 20:13:05 -0700

    reponse ip

--- json/dtc-show-ip.php
+++ json/dtc-show-ip.php
@@ -1,6 +1,22 @@
 <?php
-	
-	$data = json_decode(file_get_contents('php://input'), true);
-	echo $data;
 
+	# response format
+	#     dtcResponse=[0-9]
+	#     0 - OK
+	#     1 - IP address string two long
+
+	$data = json_decode(file_get_contents('php://input'), true);
+	if (array_key_exists('your_IPs', $data)){
+		$IPs_str = $data['your_IPs'];
+		if (strlen($IPs_str) > 10*16){
+			# 10 IP address should be enough 
+			echo "dtcResponse=1;";
+			exit("IP address string too long\n");
+		} else {
+			echo "dtcResponse=0;";
+		}
+
+	}
+	print_r($data);
+	
 ?>
