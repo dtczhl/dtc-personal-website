333d02d09d7a76e0ac84718aff47c5eade46b547
Huanle Zhang <zhanghuanle1342@gmail.com>
2017-11-02 20:16:14 -0700

    dtc-show-ip done

--- interface.php
+++ interface.php
@@ -19,7 +19,7 @@
 		<link rel="stylesheet" href="css/header.css" />
 		<link rel="stylesheet" href="css/footer.css" />
 		
-		<link rel="stylesheet" href="css/home.css" />
+		<link rel="stylesheet" href="css/interface.css" />
 		
 	</head>
 	
@@ -31,7 +31,51 @@
 		<section id="content_start">
 			
 			<!-- display -->
+			<h1 class="header_title"> Show IP Address </h1>
+			<p class="ip_description"> Good for embedded devices without monitors. Display the IP addresses of your devices at this website. Download shell script at my <a href="https://github.com/dtczhl/dtc-show-ip" target="_balnk">Github</a> </p> 
+			<table class="ip_table">
+				<tr class="ip_header_row">
+					<th class="ip_header"> Description </th>
+					<th class="ip_header"> IP Address </th>
+					<th class="ip_header"> Last Update </th> 
+				</tr>
+<?php		
+			// _account.php file defines variables for mysql account
+			//		$serverName
+			//		$userName
+			//		$password 
+			require_once('./mySql/_account.php');
+
+			global $serverName;
+			global $userName;
+			global $password;
+
+			$dbName = "dtczhl_interface";
+			$tableName = "ip_table";
+
+			$mySqli = new mysqli($serverName, $userName, $password, $dbName);
+			if ($mySqli->connect_error){
+				die("Connection failed: " . $mySqli->connect_error);
+			}
 			
+			$result = $mySqli->query('SELECT * FROM ' . $tableName);
+			if ($result->num_rows > 0){
+				while ($row = $result->fetch_assoc()){
+					echo '				';	
+					echo '<tr class="ip_row">' . "\r\n";
+					echo '				  ';
+					echo '	<td class="ip_message"> ' . $row['message'] . ' </td>' . "\r\n";
+					echo '				  ';
+					echo '	<td class="ip_ip"> ' . str_replace(" ", ";&nbsp &nbsp", $row['ip']) . ' </td>' . "\r\n";
+					echo '				  ';
+					echo '	<td class="ip_timestamp"> ' . $row['timestamp'] . ' </td>' . "\r\n";
+					echo '				';
+					echo '</tr>' . "\r\n";
+				}
+			}
+			$mySqli->close();
+?>
+		</table> <!-- end of ip table -->
 		</section>
 		
 		<?php include 'footer.php'; ?>
