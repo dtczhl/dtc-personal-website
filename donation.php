0df0ac92cbb83ecd3d67a9789d5f1f0da236410f
Huanle Zhang <zhanghuanle1342@gmail.com>
2018-01-16 08:59:48 -0800

    donation text add icon

--- donation.php
+++ donation.php
@@ -4,7 +4,7 @@ echo "\r\n";
 echo <<<EOL
 
 <div class="donation">
-	<p class="donation_description"> (: For Fun :) </p>
+	<p class="donation_description"> &#9925; For Fun &#9749; </p>
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank _top">
 		<input type="hidden" name="cmd" value="_s-xclick">
 		<input type="hidden" name="hosted_button_id" value="ZB5P5CKASJ2JL">
