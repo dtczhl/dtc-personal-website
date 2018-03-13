20e2c93506e031d364c70d743c2dbb9ea2fe0d91
Huanle Zhang <zhanghuanle1342@gmail.com>
2018-03-13 09:40:51 -0700

    hide interface

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ echo <<<EOL
 					<li id="nav_home"> <a href="index.php" class="nav"> Home <a></li>
 					<li id="nav_publications"> <a href="publications.php" class="nav"> Publications <a></li>
 					<li id="nav_resources"> <a href="resources.php" class="nav"> Resources <a></li>
-					<li id="nav_interface"> <a href="interface.php" class="nav"> Interface <a></li> 
+					<!-- <li id="nav_interface"> <a href="interface.php" class="nav"> Interface <a></li> -->
 				</ul>
 			</nav>
 		</header>
