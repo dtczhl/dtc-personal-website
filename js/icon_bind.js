a1664d21739fb42cf82cd6d1922434e3825c2b62
Huanle Zhang <zhanghuanle1342@gmail.com>
2019-07-09 17:03:50 -0400

    codacy

--- js/icon_bind.js
+++ js/icon_bind.js
@@ -1,53 +1,53 @@
 $(function(){
 
 	// pdf icon
-    var pdf_class = $('.pdf');
-    jQuery.each(pdf_class, function(){
+    var pdfClass = $(".pdf");
+    jQuery.each(pdfClass, function(){
 		$(this).prepend('<img title="Paper" src="image/icon/pdf.png" width="25" height="25" alt="pdf icon" />');
     });
 
-    // pdf icon
-    var pdf_class = $('.poster');
-    jQuery.each(pdf_class, function(){
+    // poster icon
+    var posterClass = $(".poster");
+    jQuery.each(posterClass, function(){
     $(this).prepend('<img title="Poster" src="image/icon/poster.png" width="30" height="30" alt="poster icon" />');
     });
 
 	// video icon
-	var video_class = $('.video');
-    jQuery.each(video_class, function(){
+	var videoClass = $(".video");
+    jQuery.each(videoClass, function(){
 		$(this).prepend('<img title="Video" style="margin-right:5px;" src="image/icon/video.png" width="22" height="22" alt="video icon" />');
     });
 
 	// slides icon
-	var slides_class = $('.slides');
-    jQuery.each(slides_class, function(){
+	var slidesClass = $(".slides");
+    jQuery.each(slidesClass, function(){
 		$(this).prepend('<img title="Slides" src="image/icon/slides.png" width="25" height="25" alt="slides icon" />');
     });
 
 	// award icon
-	var award_class = $('.award');
-    jQuery.each(award_class, function(){
+	var awardClass = $(".award");
+    jQuery.each(awardClass, function(){
 		$(this).prepend('<img src="image/icon/award.png" width="25" height="25" alt="award icon" />');
     });
 
-  var award_class = $('.void');
-    jQuery.each(award_class, function(){
+  var awardClass = $(".void");
+    jQuery.each(awardClass, function(){
 		$(this).prepend('<div width="28" height="28" />');
     });
 
 	// github icon
-	var github_class = $('.githubText');
-    jQuery.each(github_class, function(){
+	var githubClass = $(".githubText");
+    jQuery.each(githubClass, function(){
 		$(this).prepend('<span style="color:Blue;"> GitHub </span>');
     });
 
-	var githubIcon_class = $('.githubIconText');
-	jQuery.each(githubIcon_class, function(){
+	var githubIconClass = $(".githubIconText");
+	jQuery.each(githubIconClass, function(){
 		$(this).prepend('<img src="image/icon/github.png" width="50" alt="GitHub icon" />');
 	});
 
-  var githubIcon_class = $('.github');
-	jQuery.each(githubIcon_class, function(){
+  var githubIconClass = $(".github");
+	jQuery.each(githubIconClass, function(){
 		$(this).prepend('<img title="Source Code" style="margin-right:5px;" src="image/icon/github.png" width="25" alt="GitHub icon" />');
 	});
 
