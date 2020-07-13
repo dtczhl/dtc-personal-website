41c07a114d17ec953fa91e09f7ec0d7df6012eb6
Huanle Zhang <zhanghuanle1342@gmail.com>
2020-07-13 09:58:36 -0700

    remove university program

--- index.php
+++ index.php
@@ -86,27 +86,6 @@
 		</section>
 
 		<section>
-			<h1 class="header_title"> University Program </h1>
-
-			<div class="block_wrapper" style="width:620px;"> <!-- att -->
-				<div class="block_div_title" style="width:90px;">
-					<span class="block_title"> Member </span>
-					<span class="block_period"> (present) </span>
-				</div>
-					<div class="block_div_image" style="width:140px;">
-						<a href="https://about.att.com/sites/labs_research" target="_blank"><img src="./image/institution/att.png" width="125" class="block_img" style="margin-top:10px;"/></a>
-					</div>
-					<div class="block_div_detail" style="width:390px;">
-						<span class="block_time"> 2019.02 - 2020.02, </span>
-						<span class="block_research"> Mobile Augmented Reality </span>
-						<span class="block_institution"> AT&T Labs - Research, USA </span>
-						<span class="block_advisor_modifier"> Advisor: <i>Dr.</i> </span>
-						<a class="block_advisor_link" href="https://scholar.google.com/citations?user=BhRa314AAAAJ&hl=en" rel="external" target="_blank"> Bo Han </a>
-					</div>
-			</div>
-		</section>
-
-		<section>
 				<h1 class="header_title"> Work and Internship </h1>
 
 				<div class="block_wrapper" style="width:710px;"> <!-- at&t -->
